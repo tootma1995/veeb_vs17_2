445fe1f755f707befcc02527872330b51a0b80c2
Anna Karutina <anna.karutina@khk.ee>
2017-12-11 09:53:13 +0200

    Defineeritud ja väljastatud eesnimi

---
+++ mina.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 11.12.2017
+ * Time: 9:07
+ */
+/*
+ * Loo muutuja enda nimega, omista muutuja väärtuseks
+ * sõneline väärtus - oma Eesnimi
+ * Väljasta antud muutuja väärtus koos lisatekstiga:
+ * Minu nimi on OmaEesnimi
+ * Kasuta ka mõned HTML tagid
+*/
+$anna = 'Anna';
+// echo käsklusega
+echo 'Minu nimi on '.$anna.'.<br />';
+// print funktsiooniga
+print 'Minu nimi on '.$anna.'.<br />';
+print("Minu nimi on $anna<br />");
+?>
