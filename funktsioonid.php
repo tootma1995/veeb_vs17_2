cfef1480c2ada79f0c4f81b849e54659bbd70965
Anna Karutina <anna.karutina@khk.ee>
2017-12-15 11:55:20 +0200

    Loodud värvi genereerimine

--- funktsioonid.php
+++ funktsioonid.php
@@ -11,6 +11,24 @@
  * veergude arv ning väljastab vastava suurusega
  * tabel, mille pesad on täidetud
  * juhuarvudega vahemikus 10-99*/
+
+/*
+ * Loo funktsioon genereeriVarv mis genereerib ja tagastab
+ * põhiprogrammile genereeritud värv kujul #XXXXXX
+ * Vihje: kasuta tsüklis loodud kood ja teisenda see
+ * funktsiooniks
+ * */
+
+function genereeriVarv(){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuTaisarv = rand(0, 15);
+        $juhuHex = dechex($juhuTaisarv);
+        $varv = $varv.$juhuHex;
+    }
+    return $varv;
+}
+
 function valjastaTabel($ridadeArv, $veergudeArv){
     echo '<table border="1">';
     for($reaNumber = 1; $reaNumber <= $ridadeArv; $reaNumber++){
