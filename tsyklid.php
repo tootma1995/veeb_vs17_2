75129bf710d99624a2d37f2eef8d8d2b0cf4fe89
Anna Karutina <anna.karutina@khk.ee>
2017-12-15 10:47:49 +0200

    Arvu väljastamine on täiendatud värvi valikuga

--- tsyklid.php
+++ tsyklid.php
@@ -5,6 +5,15 @@
  * Date: 15.12.2017
  * Time: 10:29
  */
+// täienda antud lahendus nii, et paaris arvud oleks
+// punast värvi ja paaritu arvud
+// sinist värvi
+$varv = '';
 for($arv = 1; $arv <= 10; $arv++){
-    echo $arv.'<br />';
+    if($arv % 2 == 0){
+        $varv = 'red';
+    } else {
+        $varv = 'blue';
+    }
+    echo '<p style="color: '.$varv.';">'.$arv.'</p>';
 }
