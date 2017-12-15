683ed4a1c3f68e0da3e1e932590f00310d3426d2
Anna Karutina <anna.karutina@khk.ee>
2017-12-15 11:18:57 +0200

    Koostatud värvilise teksti väljastamine

---
+++ varviline_tekst.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 15.12.2017
+ * Time: 11:09
+ */
+for($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuTaisarv = rand(0, 15);
+        $juhuHex = dechex($juhuTaisarv);
+        $varv = $varv.$juhuHex;
+    }
+    echo '<font color="'.$varv.'">Värviline tekst</font><br />';
+}
